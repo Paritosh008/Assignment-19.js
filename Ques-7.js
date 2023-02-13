e06cad666e9bcb64daf09ebf7859e71a66a25832
Paritosh Chaudhary <51153681+Paritosh008@users.noreply.github.com>
2023-02-13 12:08:45 +0530

    Create Ques-7.js

---
+++ Ques-7.js
@@ -0,0 +1,9 @@
+//7.
+var p = new Promise((resolve, reject) => {
+   reject(Error('The Fails!'))
+    })
+    .catch(error => console.log(error))
+    .then(error => console.log(error))
+
+    //output: The Fails!
+  
